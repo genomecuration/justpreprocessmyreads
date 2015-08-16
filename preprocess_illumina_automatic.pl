03466274268267fe3621de578332b93373d6644b
Alexie Papanicolaou <alpapan@gmail.com>
2015-08-16 13:01:29 +1000

    autoprocess

--- preprocess_illumina_automatic.pl
+++ preprocess_illumina_automatic.pl
@@ -7,13 +7,16 @@ use warnings;
 
 # change this to find all the files from left pairs
 my @files = glob("*_1_sequence.fastq");
+#my @files = glob("*R1_001.fastq");
 
 foreach my $f (sort @files){
 	my $pair = $f;
 	# change this to grab the pair's filename by substituting something 
 	$pair=~s/_1_sequence/_2_sequence/;
+#	$pair=~s/R1_001.fastq/R2_001.fastq/;
+	next if !-s $pair || $pair eq $f;
 	# change the cmd to something you like
-	my $cmd = "preprocess_illumina.pl -cdna -paired $f $pair";
+	my $cmd = "preprocess_illumina.pl -paired $f $pair";
 	system($cmd);
 	sleep(1);
 }
